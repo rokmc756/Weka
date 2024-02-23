980d24b038ce7ce0d3639a2d2de2ea03613f81a8
Jack Moon <rokmc756@gmail.com>
2024-02-24 04:50:00 +0900

    Updated for SCB

---
+++ roles/weka/tasks.mcb/disable-cert.sh
@@ -0,0 +1,28 @@
+# https://www.notion.so/wekaio/Workaround-Custom-Certificate-Authorities-for-TLS-SSL-connections-from-WEKA-containers-42f88234379244c6b9d970ab45e9c0b4?pvs=4
+
+dnf -y install jq
+
+
+cp -f /opt/weka/dist/release/4.2.7.64.spec /opt/weka/dist/release/4.2.7.64.spec.orig
+
+jq --arg SSL_CERT_FILE /host/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem '.containers.weka.env = $ARGS.named + .containers.weka.env' /opt/weka/dist/release/4.2.7.64.spec.orig > /opt/weka/dist/release/4.2.7.64.spec
+
+grep SSL /opt/weka/dist/release/4.2.7.64.spec
+
+weka user login admin Changeme12\!\@
+
+weka status
+weka local restart
+
+cp /etc/wekaio/cacert.pem /etc/wekaio/cacert.pem.orig
+cat /etc/pki/tls/certs/ca-bundle.crt >> /etc/wekaio/cacert.pem
+mount -t wekafs | wc -l # should be zero
+systemctl restart weka-agent.service
+
+weka cloud disable && sleep 60 && weka cloud enable
+
+# Confirm that recent events are displayed.  It may take several minutes for events to be made available.
+weka events --start-time -5m
+
+# If the SSL cert needed to be put into place due to OBS connectivity issues, check the connections.
+weka fs tier s3
---
+++ scripts/install-scb-ib.sh
@@ -0,0 +1,51 @@
+# 1. Form a cluster
+# Hostnames separated by spaces
+weka cluster create asgsds01-ib asgsds02-ib asgsds03-ib asgsds04-ib asgsds05-ib asgsds06-ib asgsds07-ib asgsds08-ib --host-ips=10.50.0.1,10.50.0.2,10.50.0.3,10.50.0.4,10.50.0.5,10.50.0.6,10.50.0.7,10.50.0.8
+
+# 2. Created a cluster name
+weka cluster update --cluster-name=customer1
+
+# 3. Create a parity
+weka cluster update --data-drives=4 --parity-drives=2
+
+# 4. Add host cores
+weka cluster host cores 0 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+weka cluster host cores 1 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+weka cluster host cores 2 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+weka cluster host cores 3 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+weka cluster host cores 4 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+weka cluster host cores 5 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+weka cluster host cores 6 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+weka cluster host cores 7 16 --frontend-dedicated-cores=1 --drives-dedicated-cores=8
+
+# 5. Add disks
+# Assign drives to cluster (ie. /dev/nvme0n1, etc.)
+for i in {0..7}; do weka cluster drive add $i /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 /dev/nvme4n1;done
+
+# 6. Add Networking
+weka cluster host net add 0 --device=ib0 --ips=10.50.0.1 --netmask=24 --gateway=10.0.50.254
+weka cluster host net add 1 --device=ib0 --ips=10.50.0.2 --netmask=24 --gateway=10.0.50.254
+weka cluster host net add 2 --device=ib0 --ips=10.50.0.3 --netmask=24 --gateway=10.0.50.254
+weka cluster host net add 3 --device=ib0 --ips=10.50.0.4 --netmask=24 --gateway=10.0.50.254
+weka cluster host net add 4 --device=ib0 --ips=10.50.0.5 --netmask=24 --gateway=10.0.50.254
+weka cluster host net add 5 --device=ib0 --ips=10.50.0.6 --netmask=24 --gateway=10.0.50.254
+weka cluster host net add 6 --device=ib0 --ips=10.50.0.7 --netmask=24 --gateway=10.0.50.254
+weka cluster host net add 7 --device=ib0 --ips=10.50.0.8 --netmask=24 --gateway=10.0.50.254
+
+# 7. Dedicate hosts if applicable
+for i in {0..7} ; do weka cluster host dedicate $i on;done
+
+# 8. Enable cloud
+weka cloud enable
+
+# 9. Scan drives
+weka cluster drive scan
+
+# 10. Activate the host into the cluster
+weka cluster host activate
+
+# 11.  Activate drive
+weka cluster drive activate
+
+# 12.  Start IO
+weka cluster start-io
