0c2f9992a5e00aa4e1fb5682afe18c5ecac233fd
Jack Moon <rokmc756@gmail.com>
2024-04-03 16:01:58 +0900

    Updated for reboot handler

--- scripts/ping-test.sh
+++ scripts/ping-test.sh
@@ -1,9 +1,12 @@
 for i in `seq 1 5`
 do
 
-    sudo ping -c 1 192.168.0.14$i
-    sudo ping -c 1 192.168.1.14$i
+    sudo ping -c 1 192.168.0.18$i
+    sudo ping -c 1 192.168.1.18$i
+    sudo ping -c 1 192.168.1.17$i
+    sudo ping -c 1 192.168.1.16$i
     sudo ping -c 1 192.168.1.15$i
+    sudo ping -c 1 192.168.1.14$i
  
     #sudo ping -c 1 192.168.0.17$i
     #sudo ping -c 1 192.168.1.17$i
