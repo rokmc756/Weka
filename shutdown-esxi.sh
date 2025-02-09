e9ac4d09036b651cd9bdc667d434dd8514efc37a
Jack Moon <rokmc756@gmail.com>
2025-02-10 06:52:31 +0900

    Completed to Deploy MCB Cluster with 1:1:1 Cores and Still Continuing to Modify Playbook for New Makefile

--- shutdown-esxi.sh
+++ shutdown-esxi.sh
@@ -1,8 +1,11 @@
 GW_HOST="192.168.0.101"
-ESXi_IPs="192.168.0.231 192.168.0.101"
+# ESXi_IPs="192.168.0.231 192.168.0.101"
+
+ESXi_IPs="192.168.0.231"
 
 for i in `echo $ESXi_IPs`
 do
+
     # echo $i
 
     for ln in `cat /home/jomoon/.ssh/known_hosts | grep -n $i | cut -d : -f 1`
@@ -11,7 +14,8 @@ do
         sed -ie "$ln"d /home/jomoon/.ssh/known_hosts
     done
 
-    sshpass -p "Changeme34#$" ssh -o StrictHostKeyChecking=no root@192.168.0.231 "poweroff; halt"
+    sshpass -p "Changeme34#$" ssh -o StrictHostKeyChecking=no root@$i "poweroff; halt"
+    # sshpass -p "Changeme12!@" ssh -o StrictHostKeyChecking=no root@192.168.0.101 "poweroff; halt"
 
 done
 
